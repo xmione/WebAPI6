cc4021414b73ba899d2b2229d69aa5b5b951fec0
Solomio S. Sisante <solomiosisante@hotmail.com>
2022-08-07 17:16:17 +0800

    Added Swagger UI

--- MoneyMe.API/Program.cs
+++ MoneyMe.API/Program.cs
@@ -13,7 +13,7 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "MoneyMe.API v1"));
 }
 
 app.UseHttpsRedirection();
